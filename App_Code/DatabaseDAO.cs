e8d391308017de9d92b95de5e76b99fe6f348e61
matthew.mccormick <matthew_mccormick@hotmail.co.uk>
2017-12-15 18:39:55 +0000

    Added comments to pages

--- App_Code/DatabaseDAO.cs
+++ App_Code/DatabaseDAO.cs
@@ -8,24 +8,24 @@ using System.Web;
 /// </summary>
 public class DatabaseDAO
 {
-    private TestSiteDBEntities createDatabaseConnection()
+    private TestSiteDBEntities createDatabaseConnection() //Create a new database connection
     {
         return new TestSiteDBEntities();
     }
-    public void addContactDetails(string sendName, string emailAddress, string companyName, string subjectField, string messageField)
+    public void addContactDetails(string sendName, string emailAddress, string companyName, string subjectField, string messageField) // input fields to input
     {
 
-        ContactSentDetail contact = new ContactSentDetail();
-        contact.senderName = sendName;
-        contact.emailAddress = emailAddress;
-        contact.companyName = companyName;
-        contact.subjectField = subjectField;
-        contact.messageField = messageField;
-        contact.dateRecieved = DateTime.Now.Date;
+        ContactSentDetail contact = new ContactSentDetail(); //Set a new instance of a Contact
+        contact.senderName = sendName; //Set db value senderName equal to sendName
+        contact.emailAddress = emailAddress; //Set db value emailAddress equal to emailAddress
+        contact.companyName = companyName; //Set db companyName senderName equal to companyName
+        contact.subjectField = subjectField; //Set db subjectField senderName equal to subjectField
+        contact.messageField = messageField; //Set db messageField senderName equal to messageField
+        contact.dateRecieved = DateTime.Now.Date; //Set db value dateReceived equal to time which the POST was sent
 
         TestSiteDBEntities db = createDatabaseConnection();
-        db.ContactSentDetails1.Add(contact);
-        db.SaveChanges();
+        db.ContactSentDetails1.Add(contact); //Add the details to the contact sent details table
+        db.SaveChanges(); //save changes to the database
         //string successfulSave = "Save was successful";
     }
 }
--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,4 @@
-﻿/* Message visisbility upon contact form submission */
-
-$(document).ready(function messageVisibility() {
-    if ($("#successfulSubmission").length > 0) {
-        $('#contactForm').hide();
-        document.getElementById("messageDetails").style.display = "block";
-        $(".validationMessage").show();
-   
-    }
-});
+﻿/* CROSS-PAGE FUNCTIONALITY */
 
 /* Drop down header shown when mouse over logo*/
 
@@ -45,14 +36,121 @@ $(document).ready(function changeColourDropDownItem() {
 
 $(document).ready(function changeColourTile() {
     $('.tileIconHome').mouseover(function () {
-        
-        $(this).css({"backgroundColor":"white", "color":"black"});
+
+        $(this).css({ "backgroundColor": "white", "color": "black" });
     })
     $('.tileIconHome').mouseout(function () {
 
         $(this).css({ "backgroundColor": "", "color": "" });
     })
 })
+
+/* Homepage text transition animation */
+
+
+/* Homepage text transition */
+
+var dateHour = new Date(); //Set a new date variable
+var hour = dateHour.getHours(); // Get the hours of the date
+var messageHour; //Set a new message variable based on the hour
+$(document).ready(function dateMessage(callback) {
+    if (hour < 12) {
+        document.getElementById("messageGreet").innerHTML = "Good Morning" //When the hour is less than 12 then set message to Good Morning
+        var messageHour = "Good Morning.";
+    }
+    else if (hour >= 12 && hour <= 17) {
+        document.getElementById("messageGreet").innerHTML = "Good Afternoon" //When the hour is more than 12 but less than 17 then set message to Good Afternoon
+        var messageHour = "Good Afternoon";
+    }
+    else if (hour > 17 && hour < 24) {
+        document.getElementById("messageGreet").innerHTML = "Good Evening" // When hour us greater than 17 then set message to Good Evening
+        var messageHour = "Good Evening";
+    }
+    else {
+        document.getElementById("messageGreet").innerHTML = "Hello" // In case of error, then just return hello
+        messageHour = "Hello";
+    }
+    return messageHour //return the message from the statements
+    messageHour = messageHour; 
+    callback(messageHour); // call back the message hour message
+});
+
+var intLoop = 4;  //Set the integerLoop Var
+$(document).ready(function setIntervalLoop() {
+    var startUpCount = 0;  //set the start up counter, which counts how many times the message has changed
+    while (startUpCount < 1) {
+        $('#messageGreetCycle').delay(4000).fadeOut(500); //Always show first message once and only once
+        startUpCount++;
+    }
+    setInterval(function () {
+        var cycleText = [$('#messageGreet').text(), "Welcome to Matthew's Website", "Feel free to look around", "Use the Mc Icon to navigate between pages", "Contact me for more information"] //Messages to display on the page
+        if (intLoop < 4 && isRunning) {
+            intLoop++; //while the loop is less than 4 and has not been paused then continue to count
+        }
+        else if (intLoop >= 4 && isRunning) {
+            intLoop = 0 //When it reaches 4, then reset the counter to 0
+        }
+        $('#messageGreetCycle').text(cycleText[intLoop]).fadeIn().delay(4000); //Fade in the message associated with the integer loop (i.e. cycleText[1] will display the second message)
+        if (isRunning) {
+            $('#messageGreetCycle').text(cycleText[intLoop]).fadeOut(500).delay(1000); //if it is running then fade the message out as normal
+        }
+        else if (!isRunning) { 
+            $('#messageGreetCycle').show(); //If it is not running and is paused then always show the message
+        }
+
+    }, 6000); //time difference before fade-in/out
+
+});
+
+/* Stopping or resuming transitions */
+var isRunning = true; //Automatically set the transitions to true
+$(document).ready(function pauseResumeLoop() {
+    $('#pauseLoop').click(function () { //When clicking pauseloop (should change name to incorporate start) then if it is running, set run value to false and show start icon
+        if (isRunning) {
+            isRunning = false
+            document.getElementById('pauseLoop').innerHTML = "Start";
+            document.getElementById("pauseLoop").src = "/Images/startIcon.png"
+        }
+        else { //When clicking pauseloop (should change name to incorporate start) then if it is not running, set run value to true and show pause icon
+            isRunning = true;
+            document.getElementById("pauseLoop").src = "/Images/pauseIcon.png"
+            document.getElementById('pauseLoop').innerHTML = "Start";
+        }
+
+    });
+    $('#pauseLoop').mouseover(function () { //hover functions to add emphasis
+        document.getElementById('pauseLoop').style.height = "25px";
+    });
+    $('#pauseLoop').mouseout(function () { //hover functions to add emphasis
+        document.getElementById('pauseLoop').style.height = "20px";
+    });
+    $('#accessibleLink').mouseover(function () { //hover functions to add emphasis
+        document.getElementById('accessibleLink').style.fontSize = "2.15vw";
+    });
+    $('#accessibleLink').mouseout(function () { //hover functions to add emphasis
+        document.getElementById('accessibleLink').style.fontSize = "2vw";
+    });
+
+});
+
+
+
+/* PAGE-SPECIFIC FUNCTIONALITY */
+
+/* Contact Form */
+
+/* Message visisbility upon contact form submission */
+
+$(document).ready(function messageVisibility() {
+    if ($("#successfulSubmission").length > 0) { //if successful submission exists, hide contact form and display message and confirmation
+        $('#contactForm').hide(); 
+        document.getElementById("messageDetails").style.display = "block";
+        $(".validationMessage").show();
+   
+    }
+});
+
+
 
 /* change colour of links upon mouse over in contact page */
 
@@ -65,91 +163,9 @@ $(document).ready(function changeContactText() {
     })
 });
 
-/* Homepage text transition */
 
-var dateHour = new Date();
-var hour = dateHour.getHours();
-var messageHour;
-$(document).ready(function dateMessage(callback) {
-    if (hour < 12) {
-        document.getElementById("messageGreet").innerHTML = "Good Morning"
-        var messageHour = "Good Morning.";
-    }
-    else if (hour >= 12 && hour <= 17) {
-        document.getElementById("messageGreet").innerHTML = "Good Afternoon"
-        var messageHour = "Good Afternoon";
-    }
-    else if (hour > 17 && hour < 24) {
-        document.getElementById("messageGreet").innerHTML = "Good Evening"
-        var messageHour = "Good Evening";
-    }
-    else {
-        document.getElementById("messageGreet").innerHTML = "Hello"
-        messageHour = "Hello";
-    }
-    return messageHour
-    messageHour = messageHour;
-    callback(messageHour);
-});
 
-var intLoop = 4;
-$(document).ready(function setIntervalLoop() {
-    var startUpCount = 0;
-    while (startUpCount < 1) {
-            $('#messageGreetCycle').delay(4000).fadeOut(500);
-            startUpCount++;
-        }
-        setInterval(function () {
-            var cycleText = [$('#messageGreet').text(), "Welcome to Matthew's Website", "Feel free to look around", "Use the Mc Icon to navigate between pages", "Contact me for more information"]
-            if (intLoop < 4 && isRunning) {
-                intLoop++;
-            }
-            else if (intLoop >= 4 && isRunning) {
-                intLoop = 0
-            }
-            $('#messageGreetCycle').text(cycleText[intLoop]).fadeIn().delay(4000);
-            if (isRunning) {
-                $('#messageGreetCycle').text(cycleText[intLoop]).fadeOut(500).delay(1000);
-            }
-            else if (!isRunning) {
-                $('#messageGreetCycle').show();
-            }
-
-        }, 6000);
-
-});
-
-   /* Stopping or resuming transitions */ 
-     var isRunning = true;
-     $(document).ready(function pauseResumeLoop() {
-         $('#pauseLoop').click(function () {
-             if (isRunning) {
-                 isRunning = false
-                 document.getElementById('pauseLoop').innerHTML = "Start";
-                 document.getElementById("pauseLoop").src = "/Images/startIcon.png"
-             }
-             else {
-                 isRunning = true;
-                 document.getElementById("pauseLoop").src = "/Images/pauseIcon.png"
-                 document.getElementById('pauseLoop').innerHTML = "Start";
-             }
-             
-         });
-         $('#pauseLoop').mouseover(function () {
-             document.getElementById('pauseLoop').style.height = "25px";
-         });
-         $('#pauseLoop').mouseout(function () {
-             document.getElementById('pauseLoop').style.height = "20px";
-         });
-         $('#accessibleLink').mouseover(function () {
-             document.getElementById('accessibleLink').style.fontSize = "2.15vw";
-         });
-         $('#accessibleLink').mouseout(function () {
-             document.getElementById('accessibleLink').style.fontSize = "2vw";
-         });
-         
-     });
-
+/* DEPRECIATED FEATURES */
 
 
      /* Change colour depreciated -- No longer required!*/
